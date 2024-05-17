e489e5683235fbb786dae05a19b81dc61fbc4963
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2024-05-17 11:32:55 -0700

    added "showing reviews" feature.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // //Define the include function for absolute file name
 global.base_dir = __dirname;
-global.abs_path = function(path) {
+global.abs_path = function (path) {
 	return base_dir + path;
 }
-global.include = function(file) {
+global.include = function (file) {
 	return require(abs_path('/' + file));
 }
 
@@ -12,7 +12,7 @@ require("./utils.js");
 require('dotenv').config();
 const express = require('express');
 const router = include('routes/router');
-var {database} = include('databaseConnection');
+var { database } = include('databaseConnection');
 const { google } = require('googleapis');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -24,7 +24,7 @@ const bcrypt = require('bcrypt');
 const Joi = require("joi");
 
 const saltRounds = 12;
-const expireTime = 6 * 4 * 7 * 24 * 60 * 60 * 1000; //expires after 6 months  (hours * minutes * seconds * millis)
+const expireTime = 6 * 4 * 7 * 24 * 60 * 60 * 1000; //expires after 6 months (months * weeks * days * hours * minutes * seconds * millis)
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -38,6 +38,7 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 //database related variables and functions on start up
 
 const userCollection = database.db(mongodb_database).collection('users');
+const locationCollection = database.db(mongodb_database).collection('locations');
 
 var mongoStore = MongoStore.create({
 	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
@@ -79,6 +80,7 @@ app.get('/auth/google/callback', async (req, res) => {
 });
 
 const nodemailer = require('nodemailer');
+const { name } = require("ejs");
 
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
@@ -87,46 +89,58 @@ let transporter = nodemailer.createTransport({
 		pass: process.env.APP_PASSWORD // Your Google App Password
 	}
 });
- 
+
 //all the app.use
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
-app.use('/',router);
-app.use(session({ 
-    secret: node_session_secret,
+app.use('/', router);
+app.use(session({
+	secret: node_session_secret,
 	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
+	saveUninitialized: false,
 	resave: true
 }
 ));
 
 //moongose connection and review/user schemas
-mongoose.connect('mongodb+srv://maferbaltaza:XKtLlhYnXfi5KXIB@cluster0.fx7hg8i.mongodb.net/users', {
-
+mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log('Connected to MongoDB');
+	console.log('Connected to MongoDB');
 });
 
 const reviewSchema = new mongoose.Schema({
-    text: { type: String, default: "" },
-    date: { type: Date, default: Date.now }
+	text: { type: String, default: "" },
+	starRating: { type: Number, default: 0 },
+	user: { type: String, default: "" },
+	date: { type: Date, default: Date.now }
 });
 
 const userSchema = new mongoose.Schema({
 	username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    reviews: { type: [reviewSchema], default: [{ text: "" }] },
+	email: { type: String, required: true, unique: true },
 	profilePicture: {
-        data: Buffer,
-        contentType: String
-    },
+		data: Buffer,
+		contentType: String
+	},
+});
+const locationSchema = new mongoose.Schema({
+	name: { type: String, required: true, unique: true },
+	description: { type: String, required: true },
+	reviews: { type: [reviewSchema], default: [{ text: "" }] },
+	picture: {
+		data: Buffer,
+		contentType: String
+	},
 });
 
 const User = mongoose.model('User', userSchema);
+const Location = mongoose.model('Location', locationSchema);
 
 
 
@@ -174,33 +188,34 @@ async function getUserName(req) {
 		var email = req.session.email;
 		const result = await userCollection.find({ email: email }).project({ username: 1, _id: 1 }).toArray();
 		return result[0].username;
-	}else {
+	} else {
 		return null;
 	}
 }
 
 //This block of code to do sign up, login, log out, and home page is from COMP2537 assignment 2 and modified to fit the project
-app.get('/', async(req,res) => {
+app.get('/', async (req, res) => {
 	if (req.session.authenticated) {
-		res.render("index", { user: await getUserName(req)});
+		res.render("index", { user: await getUserName(req) });
 
 	} else {
-		res.render("index", { user: null});
-	}});
-
-app.get('/createUser', (req,res) => {
-    res.render("createUser");
+		res.render("index", { user: null });
+	}
 });
 
-app.get('/login', (req,res) => {
+app.get('/createUser', (req, res) => {
+	res.render("createUser");
+});
+
+app.get('/login', (req, res) => {
 	res.render("login");
 });
 
-app.post('/createUser', async (req,res) => {
-    var username = req.body.username;
+app.post('/createUser', async (req, res) => {
+	var username = req.body.username;
 	var email = req.body.email;
 	var password = req.body.password;
-	
+
 	const schema = Joi.object(
 		{
 			username: Joi.string().alphanum().max(20).required(),
@@ -208,15 +223,15 @@ app.post('/createUser', async (req,res) => {
 			password: Joi.string().max(20).required()
 		});
 	const validationResult = schema.validate({ username, email, password });
-	
+
 	if (validationResult.error != null) {
 		var msg = validationResult.error.message;
 		res.redirect("/incorrectInput?message=" + msg + "&previousPage=createUser");
 		return;
 	}
-	
+
 	var hashedPassword = await bcrypt.hash(password, saltRounds);
-	
+
 	await userCollection.insertOne({ username: username, email: email, password: hashedPassword, reviews: [{ text: "" }] });
 	req.session.authenticated = true;
 	req.session.email = email;
@@ -227,35 +242,33 @@ app.post('/createUser', async (req,res) => {
 
 
 app.post('/post_review', async (req, res) => {
-
 	try {
-
+		let name = req.query.location;
+		var description = "blah blah blah";
 		const email = req.session.email;
-        
-		const {opinion } = req.body;
-
-        // Find the user by email or create a new one
-        let user = await User.findOne({ email }); 
-
+		var user = await getUserName(req);
+		const { opinion } = req.body;
+		const { starRating } = req.body;
+		// Find the user by email or create a new one
+		let l = await Location.findOne({ "name": name });
+		console.log(name);
 		// Ensure the reviews array is initialized 
-        if (!user) {
-            user = new User({ email, reviews: [{ text: "" }] });
-        }
+		if (!l) {
+			l = new Location({ name, description, reviews: [{ text: "" }] });
+		}
+		// Add the new review to the reviews array
+		l.reviews.push({ text: opinion, starRating: starRating, user: user, date: Date.now() });
 
-		console.log(user);
-        // Add the new review to the reviews array
-        user.reviews.push({ text: opinion });
+		// Save the user
+		await l.save();
 
-        // Save the user
-        await user.save();
-       
-		res.status(200).send('Review saved successfully');
+		res.status(200).redirect('/review?location=' + name);
 
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Failed to update review');
-    }
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Failed to update review');
+	}
 
 
 });
@@ -263,11 +276,11 @@ app.post('/post_review', async (req, res) => {
 
 app.get('/incorrectInput', (req, res) => {
 	var msg = req.query.message;
-	res.render("submitUser", { msg: msg ,previousPage: req.query.previousPage});
+	res.render("submitUser", { msg: msg, previousPage: req.query.previousPage });
 });
 
-app.post('/login', async (req,res) => {
-    var email = req.body.email;
+app.post('/login', async (req, res) => {
+	var email = req.body.email;
 	var password = req.body.password;
 	const schema = Joi.object(
 		{
@@ -276,12 +289,12 @@ app.post('/login', async (req,res) => {
 		});
 	const validationResult = schema.validate({ email, password });
 	if (validationResult.error != null) {
-		res.redirect("/incorrectInput?message=invalid email or password"+"&previousPage=login");
+		res.redirect("/incorrectInput?message=invalid email or password" + "&previousPage=login");
 		return;
 	}
 	const result = await userCollection.find({ email: email }).project({ username: 1, email: 1, password: 1, _id: 1 }).toArray();
 	if (result.length != 1) {
-		res.redirect("/incorrectInput?message=user not found"+"&previousPage=login");
+		res.redirect("/incorrectInput?message=user not found" + "&previousPage=login");
 		return;
 	}
 	if (await bcrypt.compare(password, result[0].password)) {
@@ -292,73 +305,75 @@ app.post('/login', async (req,res) => {
 		return;
 	}
 	else {
-		res.redirect("/incorrectInput?message=incorrect password"+"&previousPage=login");
+		res.redirect("/incorrectInput?message=incorrect password" + "&previousPage=login");
 		return;
 	}
 });
 
-app.get('/loggingin', (req,res) => {
-    res.render("loggingin");
+app.get('/loggingin', (req, res) => {
+	res.render("loggingin");
 });
 
-app.get('/logout', (req,res) => {
+app.get('/logout', (req, res) => {
 	req.session.destroy();
-    res.redirect("/");
+	res.redirect("/");
 });
 
-app.get('/info', (req,res) => {
-    res.render("info");
+app.get('/info', (req, res) => {
+	res.render("info");
 });
 
-app.get('/about_us', (req,res) => {
-    res.render("about_us");
+app.get('/about_us', (req, res) => {
+	res.render("about_us");
 });
 
-app.get('/destination', (req,res) => {
-    res.render("destination");
+app.get('/destination', async (req, res) => {
+	var locationName = req.query.location;
+	var location = await locationCollection.find({ name: locationName }).project({ name: 1, description: 1, reviews: 1, _id: 1 }).toArray();
+	res.render("destination", { location: location[0] });
 });
 
-app.get('/home', (req,res) => {
-    res.render("home");
+
+app.get('/home', async (req, res) => {
+	const result = await locationCollection.find().project({ name: 1, description: 1, reviews: 1, _id: 1 }).toArray();
+	res.render("home", { locations: result });
 });
 
-app.get('/post_review', async (req,res) => {
-
+app.get('/post_review', async (req, res) => {
+	var locationName = req.query.location;
 	try {
 		const username = await getUserName(req);
+		const email = req.session.email;
+		const user = await User.findOne({ email });
 
-        const email = req.session.email;
-        const user = await User.findOne({ email });
-		
-		console.log(username);
+		res.render('post_review', {
+			user: user,
+			username: username,
+			location: locationName
+		});
 
-        res.render('post_review', {
-            user: user,
-			username: username
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal server error');
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Internal server error');
 	}
-	
+
 });
 
 
 // This section allows the user to set their profile picture and is from geeksforgeeks website(https://www.geeksforgeeks.org/upload-and-retrieve-image-on-mongodb-using-mongoose/)
-app.get('/profile', async(req,res) => {
-	
+app.get('/profile', async (req, res) => {
+
 	var imgSrc = await userCollection.find({ email: req.session.email }).project({ image_id: 1, _id: 0 }).toArray();
-    res.render('profile',{ user: await getUserName(req), email: req.session.email ,imgSrc:imgSrc[0].image_id});
+	res.render('profile', { user: await getUserName(req), email: req.session.email, imgSrc: imgSrc[0].image_id });
 
 });
 
 
-app.get('/setting', (req,res) => {
+app.get('/setting', (req, res) => {
 	res.render("setting");
 });
 
-app.post('/changePersonalinfo', async(req,res) => {
+app.post('/changePersonalinfo', async (req, res) => {
 	var username = req.body.username;
 	var password = req.body.newpassword;
 	var curentPassword = req.body.curpassword;
@@ -367,25 +382,27 @@ app.post('/changePersonalinfo', async(req,res) => {
 			username: Joi.string().alphanum().max(20).required(),
 			password: Joi.string().max(20).required()
 		});
-	const validationResult = schema.validate({ username , password});
+	const validationResult = schema.validate({ username, password });
 	if (validationResult.error != null) {
 		var msg = validationResult.error.message;
-		res.redirect("/incorrectInput?message=" + msg+"&previousPage=setting");
+		res.redirect("/incorrectInput?message=" + msg + "&previousPage=setting");
 		return;
 	}
 	var currentPassword = await userCollection.find({ email: req.session.email }).project({ password: 1, _id: 0 }).toArray();
-	if(await bcrypt.compare(curentPassword, currentPassword[0].password)) {
+	if (await bcrypt.compare(curentPassword, currentPassword[0].password)) {
 		var hashedPassword = await bcrypt.hash(password, saltRounds);
-	await userCollection.updateOne({ email: req.session.email }, { $set: { username: username, password: hashedPassword } });
-	res.redirect('/profile');
-	}else {
-		res.redirect("/incorrectInput?message=incorrect password"+"&previousPage=setting");
+		await userCollection.updateOne({ email: req.session.email }, { $set: { username: username, password: hashedPassword } });
+		res.redirect('/profile');
+	} else {
+		res.redirect("/incorrectInput?message=incorrect password" + "&previousPage=setting");
 	}
 });
 
 
-app.get('/review', (req,res) => {
-    res.render("review");
+app.get('/review', async (req, res) => {
+	var locationName = req.query.location;
+	var location = await locationCollection.find({ name: locationName }).project({ name: 1, description: 1, reviews: 1, _id: 1 }).toArray();
+	res.render("review", { location: location[0], reviews: location[0].reviews });
 });
 
 
@@ -503,10 +520,10 @@ app.post('/reset-password/:token', async (req, res) => {
 	res.send('Your password has been updated successfully.');
 });
 
-app.get("*", (req,res) => {
+app.get("*", (req, res) => {
 	res.status(404).render("404");
 })
 
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
+	console.log("Node application listening on port " + port);
 }); 
--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,6 @@
 for(var i=0; i<2; i++){
 document.getElementById('location'+ i).addEventListener('click', function(){
+    var location =
 window.location.href = 'destination';    
 });
 }
